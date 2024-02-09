796755e7f54394887312607a68bdbcd34ac33bf5
Lucas Henrique Santos Amaral <lucasahenriqueamaral@gmail.com>
2024-02-09 13:32:17 -0400

    add: rota que recupera fotos de perfil

--- server.js
+++ server.js
@@ -1,73 +1,111 @@
-const express = require('express');
-const client = require('./whatsapp');
-const cors = require('cors');
+const express = require("express");
+const client = require("./whatsapp");
+const cors = require("cors");
 
 const zapClient = client.client;
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
+const cacheImages = {};
+const profilePhoto = {};
 const port = 3003;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.get('/me/info', async (req, res) => {
-    try {
-        const me = await zapClient.info;
-        res.json(me);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-
+app.get("/me/info", async (req, res) => {
+  try {
+    const me = zapClient.info;
+    res.json(me);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
-app.get('/chats', async (req, res) => {
-    try {
-        const chats = await zapClient.getChats();
-        res.json(chats);
-    } catch (error) {
-
-    }
-
+app.get("/getContacts", async (req, res) => {
+  try {
+    const contacts = await zapClient.getContacts();
+    res.json(contacts);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
-app.get('/chats/:id/messages', async (req, res) => {
-    try {
-        const chat = await zapClient.getChatById(req.params.id);
-        const messages = await chat.fetchMessages({
-            limit: 10
-        });
-        res.json(messages);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-
+app.get("/chats", async (req, res) => {
+  try {
+    const chats = await zapClient.getChats();
+    const firstsChats = chats.slice(0, 20);
+    res.json(firstsChats);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
-app.post('/send', async (req, res) => {
-    try {
-        const { to, message } = req.body;
-        const chat = await zapClient.getChatById(to);
-        chat.sendMessage(message);
-        res.json({ to, message });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+app.get("/chats/:id/messages", async (req, res) => {
+  try {
+    const chat = await zapClient.getChatById(req.params.id);
+    const messages = await chat.fetchMessages({
+      limit: 12,
+    });
+    res.json(messages);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
-})
+app.post("/send", async (req, res) => {
+  try {
+    const { to, message } = req.body;
+    const chat = await zapClient.getChatById(to);
+    chat.sendMessage(message);
+    res.json({ to, message });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 app.get("/chats/:id", async (req, res) => {
-    try {
-        const chat = await zapClient.getChatById(req.params.id);
-        res.json(chat);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
+  try {
+    const chat = await zapClient.getChatById(req.params.id);
+    res.json(chat);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/message/donwloadMedia/:id", async (req, res) => {
+  try {
+    const message = await zapClient.getMessageById(req.params.id);
+
+    if (cacheImages[req.params.id]) {
+      res.json(cacheImages[req.params.id]);
+      return;
     }
 
+    const media = await message.downloadMedia();
+    cacheImages[req.params.id] = media;
+    res.json(media);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/profilePhoto/:id", async (req, res) => {
+  try {
+    if (profilePhoto[req.params.id]) {
+      return res.json(profilePhoto[req.params.id]);
+    }
+
+    const photo = await zapClient.getProfilePicUrl(req.params.id);
+    profilePhoto[req.params.id] = photo;
+    res.json(photo);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 });
--- whatsapp.js
+++ whatsapp.js
@@ -1,33 +1,31 @@
-const { Client, LocalAuth } = require('whatsapp-web.js');
-const qrcode = require('qrcode-terminal');
+const { Client, LocalAuth } = require("whatsapp-web.js");
+const qrcode = require("qrcode-terminal");
 const client = new Client({
-    authStrategy: new LocalAuth({ clientId: "LucasPrincipall"})
+  authStrategy: new LocalAuth({ clientId: "Luc" }),
 });
 
-
-client.on('qr', (qr) => {
-    qrcode.generate(qr, { small: true });
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
 });
 
-client.on('ready', async() => {
-    console.log('Client is ready!');
-    
-    //get last chats
-    // const chats = await client.getChats();
+client.on("ready", async () => {
+  console.log("Client is ready!");
 
-    //get last messages
-    // const messages = await chats[0].fetchMessages({limit: 10});
-    // for (let message of messages) {
-    //     console.log(message.body);
-    // }
+  //get last chats
+  // const chats = await client.getChats();
 
-    //get my info
+  //get last messages
+  // const messages = await chats[0].fetchMessages({limit: 10});
+  // for (let message of messages) {
+  //     console.log(message.body);
+  // }
+
+  //get my info
 });
 
-client.on('message', async (message) => {
-
-    console.log(message)
-    console.log(`user: ${message.from} - message: ${message.body}`)
+client.on("message", async (message) => {
+  console.log(message);
+  console.log(`user: ${message.from} - message: ${message.body}`);
 });
 
 client.initialize();
